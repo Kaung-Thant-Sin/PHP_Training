8fea7b0d044f284a08394ba2cd3da6683c7283e8
kaungthantsin123 <119910012+kaungthantsin123@users.noreply.github.com>
2022-12-07 15:34:33 +0630

    first commit

--- Tutorial1/index.php
+++ Tutorial1/index.php
@@ -5,21 +5,57 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Tutorial1</title>
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-  <?php 
+<?php
+
+echo "<h1>Tutorial 1</h1><br>";
+
 $x = 0;
 $y = 0;
-while($x<8)
-{
 
-  while($y<8)
+echo "<table>";
+
+while ($x < 8)
+{
+  echo "<tr>";
+
+  while ($y < 8)
   {
-  echo "hello";
-   $y++;
+    if (($x % 2) == 0)
+    {  
+        if (($y % 2) == 0)
+        {  
+            echo "<td class='box2'></td>";  
+        } else
+        {  
+            echo "<td class='box1'></td>";  
+        }    
+    } else
+    {  
+        if (($y % 2) == 0)
+        {  
+            echo "<td class='box1'></td>";  
+        } else
+        {  
+            echo "<td class='box2'></td>";  
+        }   
+    }  
+
+    $y++;
+
   }
-$x++;
+
+  echo "</tr>";
+  
+  $y=0;
+  $x++;
+
 }
-  ?>
+
+echo "</table>";
+
+?>
 </body>
 </html>
