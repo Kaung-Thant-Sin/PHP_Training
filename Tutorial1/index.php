faed8aacf656904e34e0f37a47b3612e0b29953f
kaungthantsin123 <119910012+kaungthantsin123@users.noreply.github.com>
2022-12-07 10:17:26 +0630

    fisrst commit

---
+++ Tutorial1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tutorial1</title>
+</head>
+<body>
+  <?php 
+$x = 0;
+$y = 0;
+while($x<8)
+{
+
+  while($y<8)
+  {
+  echo "hello";
+   $y++;
+  }
+$x++;
+}
+  ?>
+</body>
+</html>
